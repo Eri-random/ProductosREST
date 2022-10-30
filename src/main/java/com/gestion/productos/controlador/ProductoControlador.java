4530eec9f22950af5a0eaa5ac027acd969aed34a
Eri-random <brenherrera@alumno.unlam.edu.ar>
2022-10-29 22:55:49 -0300

    practica RESTful

---
+++ src/main/java/com/gestion/productos/controlador/ProductoControlador.java
@@ -0,0 +1,57 @@
+package com.gestion.productos.controlador;
+
+import com.gestion.productos.modelo.Producto;
+import com.gestion.productos.servicio.ProductoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductoControlador {
+
+    @Autowired
+    private ProductoServicio servicio;
+
+    @GetMapping("/productos")
+    public List<Producto> listarProducto(@RequestHeader("Autorizador") String token){
+        return servicio.listarProductos();
+    }
+
+    @GetMapping("/productos/{id}")
+    public ResponseEntity<Producto> obtenerProducto(@PathVariable Integer id){
+        try{
+            Producto producto = servicio.obtenerProductoPorID(id);
+            return new ResponseEntity<Producto>(producto,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<Producto>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/productos")
+    public void guardarProducto(@RequestBody Producto producto){
+        servicio.guardarProducto(producto);
+    }
+
+    @PutMapping("/productos/{id}")
+    public ResponseEntity<?> actualizarProducto(@RequestBody Producto producto, @PathVariable Integer id){
+        try {
+            Producto productoExistente = servicio.obtenerProductoPorID(id);
+
+            productoExistente.setNombre(producto.getNombre());
+            productoExistente.setPrecio(producto.getPrecio());
+
+            servicio.guardarProducto(productoExistente);
+            return new ResponseEntity<Producto>(HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<Producto>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/productos/{id}")
+    public void eliminarProducto(@PathVariable Integer id){
+        servicio.eliminarProducto(id);
+    }
+}
